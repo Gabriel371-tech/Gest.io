5472ae14cd8d0c8d4e1389a72790712cdb4b355e
Gabriel371-tech <gabriel.lopes176@etec.sp.gov.br>
2025-10-24 21:47:06 -0300

    first commit

---
+++ src/test/java/br/com/io/Gest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.io.Gest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
